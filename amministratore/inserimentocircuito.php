3d3c89abdef5f0e4bdfe2eb33ce5c2c9904dbf0c
lucafinotello <luca.finotello93@gmail.com>
2020-07-28 13:40:23 +0200

    allineamento

--- amministratore/inserimentocircuito.php
+++ amministratore/inserimentocircuito.php
@@ -4,16 +4,12 @@ include("db_con.php");
 include_once('mysql-fix.php');
 include ('header.html');
 ?>
-
     <div id="main">
         <?php include ('menuDesktopAmministratore.html');?>
 		<div id="contenuto">
 			<?php
-
-
             function check_ext($tipo)
             {
-
                 switch ($tipo) {
                     case "image/png":
                         return true;
@@ -31,7 +27,6 @@ include ('header.html');
                         return false;
                         break;
                 }
-
             }
 
             function get_ext($tipo)
@@ -54,12 +49,10 @@ include ('header.html');
                         return false;
                         break;
                 }
-
             }
 
             function get_error($tmp, $type, $size, $max_size)
             {
-
                 if (!is_uploaded_file($tmp)) {
                     echo "File caricato in modo non corretto<br />";
                 }
@@ -70,17 +63,13 @@ include ('header.html');
                     echo "Dimensione del file troppo grande<br />";
                 }
                 echo '<a href="/uploader/index.php">Torna all\'uploader</a>';
-
             }
-
             $tmp = $_FILES['image']['tmp_name'];
             $type = $_FILES['image']['type'];
             $size = $_FILES['image']['size'];
             $max_size = 5242880; //dimensione massima in byte consentita
             $folder = "../immagini/"; //cartella di destinazione dell'immagine
-
             if (is_uploaded_file($tmp) && check_ext($type) && $size <= $max_size) {
-
                 $ext = get_ext($type); //estensione dell'immagine
                 $name = time() . rand(0, 999); //timestamp attuale + un numero random compreso tra 0 e 999
                 $name = $name . $ext; //aggiungo al nome appena creato l'estensione
@@ -92,10 +81,12 @@ include ('header.html');
                     $stato = $_POST["stato"];
                     $paese = $_POST["paese"];
                     $descrizione = $_POST["descrizione"];
-                    $strsql = "insert into circuiti (circuito, immagine, stato, paese, descrizione) values ('$circuito', '$immagine', '$stato', '$paese', '$descrizione')";
+                    $strsql = "insert into circuiti set circuito= '$circuito', immagine= '$immagine', stato= '$stato', 
+                                paese= '$paese', descrizione= '$descrizione'";
                     $risultato = mysqli_query($conn, $strsql);
                     if (! $risultato)
                     {
+                        print_r($_POST);
                         echo "Errore nel comando SQL" . "<br>";
                     }
                     else
@@ -107,13 +98,8 @@ include ('header.html');
                     echo '<a href="inserimentocircuiti.php">Torna all\'uploader</a>';
                 }
             } else {
-
                 get_error($tmp, $type, $size, $max_size);
-
             }
-
-
-
 			?>
 		</div>
 	</div>
